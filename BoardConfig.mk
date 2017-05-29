a183f9cc74dccf25dbc41b0314dfb49ed9fc0d30
Jacob McSwain <jacob.a.mcswain@gmail.com>
2017-05-28 22:18:17 -0500

    vs995->ls997

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,18 +14,18 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/lge/vs995
+DEVICE_PATH := device/lge/ls997
 
 # inherit from common v20
 -include device/lge/v20-common/BoardConfigCommon.mk
 
-TARGET_OTA_ASSERT_DEVICE := vs995,us996,US996
+TARGET_OTA_ASSERT_DEVICE := ls997,us996,US996
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_vs995_defconfig
+TARGET_KERNEL_CONFIG := lineageos_ls997_defconfig
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # inherit from the proprietary version
--include vendor/lge/vs995/BoardConfigVendor.mk
+-include vendor/lge/ls997/BoardConfigVendor.mk
--- deso.mk
+++ deso.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/vs995/full_vs995.mk)
+$(call inherit-product, device/lge/ls997/full_ls997.mk)
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/deso/common.mk)
@@ -6,11 +6,10 @@ $(call inherit-product, vendor/deso/common.mk)
 # Overlays (inherit after vendor/aokp to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := deso_vs995
-
+PRODUCT_NAME := deso_ls997
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v20" \
-    PRODUCT_NAME="elsa_vzw_us" \
-    BUILD_FINGERPRINT="lge/elsa_vzw/elsa:7.0/NRD90M/1628409164d39:user/release-keys" \
-    PRIVATE_BUILD_DESC="elsa_vzw-user 7.0 NRD90M 1628409164d39 release-keys"
+    PRODUCT_NAME="elsa_spr_us" \
+    BUILD_FINGERPRINT="lge/elsa_spr_us/elsa:7.0/NRD90M/1626515087a95:user/release-keys" \
+    PRIVATE_BUILD_DESC="elsa_spr_us-user 7.0 NRD90M 1626515087a95 release-keys"
